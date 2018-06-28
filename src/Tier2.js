996a0bb43e10dd375e3afbb5d4e9ff5c0a3b6640
github username <github email address>
2018-06-28 14:47:32 -0400

    Done.

--- src/Tier2.js
+++ src/Tier2.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { getReducedColor } from './randomColorGenerator.js'
+import { getReducedColor, getRandomColor } from './randomColorGenerator.js'
 import Tier3 from './Tier3'
 
 
@@ -12,13 +12,43 @@ export default class Tier2 extends Component {
     }
   }
 
+  componentWillReceieveProps(nextProps) {
+    this.setState({
+      ...this.state,
+      color: nextProps.color,
+      childColor: getReducedColor(this.nextProps.color)
+    })
+  }
+
+  // now we need to handle the click event
+  handleClick = (e) => {
+    let newColor = getRandomColor()
+    e.stopPropagation()
+    // prevents any parent event handlers from being executed.
+    this.setState({
+      ...this.state,
+      color: newColor,
+      childColor: getReducedColor(newColor)
+  });
+}
+
+  handleChildClick = (e) => {
+    e.stopPropagation()
+    this.setState({
+      ...this.state,
+      childColor: getRandomColor()
+    })
+  }
+
+
   render() {
     // hard coded color values have been added below, though they won't be
     // present in our solution. What should they be replaced with?
+    // replace with states and add onClick
     return (
-      <div className="tier2" style={{backgroundColor: this.props.color, color: this.props.color}}>
-        <Tier3 color={"#0F0"} />
-        <Tier3 color={"#F00"} />
+      <div className="tier2" style={{backgroundColor: this.state.color, color: this.state.color}} onClick={this.handleClick}>
+        <Tier3 color={this.state.childColor} handleChildClick={this.handleChildClick}/>
+        <Tier3 color={this.state.childColor} handleChildColor={this.handleChildClick}/>
       </div>
     )
   }
